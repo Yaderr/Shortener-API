1adf4825f7ac2497727fa41fc0fab3cac8580856
yaderson <yadersonjesuspatinovidal@gmail.com>
2020-10-26 18:08:01 -0500

    add time zone

--- v1/routes/version/index.js
+++ v1/routes/version/index.js
@@ -2,7 +2,7 @@ exports.default = async function (fastify, opts) {
     fastify.get('/version', async (req, reply) => {
         reply.send({
             version: 0.01,
-            releaseDate: new Date()
+            releaseDate: new Date().toLocaleString()
         })
     })
 }
